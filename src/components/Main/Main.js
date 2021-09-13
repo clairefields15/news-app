26c7b8f75c88e3b496db090dce6eb6240e8b7561
Claire Fields <clairefields15@gmail.com>
2021-09-13 16:45:49 -0600

    Store API key in .env file

--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -3,6 +3,7 @@ import './Main.css';
 import { Sidebar } from '../Sidebar/Sidebar';
 import { Articles } from '../Articles/Articles';
 import { fetchTopStories } from '../../utils/apiCalls';
+import { ErrorComponent } from '../ErrorComponent/ErrorComponent';
 
 const section = 'home';
 
@@ -25,8 +26,14 @@ export const Main = () => {
 
   return (
     <main className='main-content-container'>
-      <Sidebar />
-      <Articles articles={articles} />
+      {errorMessage && <ErrorComponent message={errorMessage} />}
+      {!errorMessage && !articles.length && <p>Loading articles...</p>}
+      {!errorMessage && !!articles.length && (
+        <>
+          <Sidebar />
+          <Articles articles={articles} />
+        </>
+      )}
     </main>
   );
 };
