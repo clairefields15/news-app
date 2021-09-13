153ce7e3f6011faca35128da9f0c22607ce51af1
Claire Fields <clairefields15@gmail.com>
2021-09-13 12:16:00 -0600

    Add undefined route and link home

---
+++ src/components/ErrorComponent/ErrorComponent.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import { NavLink } from 'react-router-dom';
+import './ErrorComponent.css';
+
+export const ErrorComponent = ({ message }) => {
+  return (
+    <>
+      <p>{message}</p>
+      {message === "Sorry, that page doesn't exist" && (
+        <>
+          <p>Would you like to go home?</p>
+          <NavLink to='/'>Home</NavLink>
+        </>
+      )}
+    </>
+  );
+};
