ed196a893e1d223cb8ca022965ea440d2175bcbf
Claire Fields <clairefields15@gmail.com>
2021-09-13 12:34:26 -0600

    Render header, navlink to go home only from other paths

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,10 +1,18 @@
 import React from 'react';
+import { NavLink, useLocation } from 'react-router-dom';
 import './Header.css';
 
 export const Header = () => {
+  const { pathname } = useLocation();
   return (
     <header>
-      <h1>THE NEWS</h1>
+      {pathname === '/' ? (
+        <h1>THE NEWS</h1>
+      ) : (
+        <NavLink to='/' className='navlink'>
+          <h1>THE NEWS</h1>
+        </NavLink>
+      )}
     </header>
   );
 };
