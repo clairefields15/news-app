ea0aae830aa956fa7287e0ca195da63f442475fb
Claire Fields <clairefields15@gmail.com>
2021-09-13 12:59:16 -0600

    Add basic grid layout for sidebar

---
+++ src/components/Sidebar/Sidebar.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import './Sidebar.css';
+
+export const Sidebar = () => {
+  return (
+    <aside>
+      <h2>Sidebar</h2>
+      <ul>
+        <li>Hello</li>
+        <li>Hello</li>
+        <li>Hello</li>
+        <li>Hello</li>
+        <li>Hello</li>
+        <li>Hello</li>
+        <li>Hello</li>
+        <li>Hello</li>
+        <li>Hello</li>
+      </ul>
+    </aside>
+  );
+};
