d9a98da7d572f2313603d0a07474203dd5a77630
Claire Fields <clairefields15@gmail.com>
2021-09-13 12:08:32 -0600

    Flesh out component structure, render main

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import { Route, Switch, NavLink, Link } from 'react-router-dom';
+import { Main } from '../Main/Main';
 
 import './App.css';
 
 export const App = () => {
   return (
     <Switch>
-      <Route exact path='/' render={() => <Home />} />
+      <Route exact path='/' render={() => <Main />} />
     </Switch>
   );
 };
