443f1f1d39f1e0211ea6db22c8c011ae9fde52e5
Claire Fields <clairefields15@gmail.com>
2021-09-13 19:22:01 -0600

    Change image urls from thumbnails to full size and style

--- src/components/Article/Article.js
+++ src/components/Article/Article.js
@@ -10,7 +10,16 @@ export const Article = ({ article }) => {
     >
       <h3>{article.title}</h3>
       <h4>{article.byline}</h4>
-      <img src={article.thumbnail} alt={article.alt} className='thumbnail' />
+      {article.imageUrl && (
+        <div className='img-container'>
+          <img
+            src={article.imageUrl}
+            alt={article.title}
+            className='thumbnail'
+          />
+        </div>
+      )}
+      {!article.imageUrl && <p>{article.abstract}</p>}
     </NavLink>
   );
 };
